2eeebceaad8a568e67e5e866341f7a26a6b170f8
camilo1210 <camilopavasrios@gmail.com>
2024-03-28 16:16:29 -0500

    inicio main

--- src/main/java/main/RoundRobin.java
+++ src/main/java/main/RoundRobin.java
@@ -2,8 +2,15 @@
 
 package main;
 import controlador.ControladorProcesarGUI;
+import vista.ProcesarGUI;
 
 
 public class RoundRobin {
+    public static void main(String[] args) {
+        
+        ProcesarGUI control = new ProcesarGUI();
+        
+        control.setVisible(true);
+    }
     
 }
