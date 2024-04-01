72f32b87395ea9cba00eba6a0598418d11548f0f
camilo1210 <camilopavasrios@gmail.com>
2024-03-31 22:28:32 -0500

    almenos ya hace algo

--- src/main/java/controlador/ControladorProcesarGUI.java
+++ src/main/java/controlador/ControladorProcesarGUI.java
@@ -9,64 +9,52 @@ import vista.ProcesarGUI;
 
 public class ControladorProcesarGUI implements ActionListener {
 
-    private ProcesarGUI procesitoGUI; //vista de la interzas
-    private Gestion procesitoGestion;//modelo.gestion
-    private Proceso procesitoModelo;// modelo.proceso
-    private DefaultTableModel modeloTabla; // Modelo de la tabla
+    private ProcesarGUI procesitoGUI;
+    private Gestion procesitoGestion;
+    private Proceso procesitoModelo;
+    private DefaultTableModel modeloTabla;
 
     // Constructor
     public ControladorProcesarGUI() {
-        this.procesitoModelo = new Proceso(); //instancia de modelo.proceso
-        this.procesitoGestion = new Gestion(); // instancia de modelo.gestion
-        this.procesitoGUI = new ProcesarGUI();//instancia de vista.procesarGUI
-        this.procesitoGUI.setVisible(true);//hacer visible la intefaz
-        this.procesitoGUI.jbt_agregar.addActionListener(this);//el escucha del boton agregar
-        this.procesitoGUI.jbt_iniciar.addActionListener(this);//el escuha del boton incio
-        this.modeloTabla = (DefaultTableModel) this.procesitoGUI.jtb_lisProcesos.getModel(); // Obtener el modelo de la tabla
-
-    }
-// eventpos de los botones 
-
-   @Override
-public void actionPerformed(ActionEvent e) {
-    if (e.getSource() == this.procesitoGUI.jbt_agregar) {
-        // Crear una nueva instancia de Proceso
-        Proceso nuevoProceso = new Proceso();
-
-        // Configurar los valores del nuevo proceso
-        nuevoProceso.setNombreProceso(this.procesitoGUI.jtf_nombreP.getText());
-        //nuevoProceso.setRafagaCPU((int) this.procesitoGUI.jSRafaCPU.getValue());
-        // Aquí debes establecer el valor del quantum al proceso cuando se coloque sea el jtf o el js
-//        nuevoProceso.setQuantum(/* Obtiene el valor del quantum */);
-
-        // Agregar el nuevo proceso a la lista de procesos en Gestion
-        this.procesitoGestion.agregarProceso(nuevoProceso);
-
-        // Mostrar mensaje sin cerrar la ventana principal
-        javax.swing.JOptionPane.showMessageDialog(this.procesitoGUI, "Proceso agregado con éxito: " + this.procesitoGUI.jtf_nombreP.getText());
-
-        // Limpiar campos después de agregar el proceso
-        this.procesitoGUI.jtf_nombreP.setText("");
-        //this.procesitoGUI.jSRafaCPU.setValue(0);
-
-        // Agregar el nuevo proceso a la tabla
-        this.procesitoGestion.agregarProcesoATabla(modeloTabla, nuevoProceso);
-
-    }
-    if (e.getSource() == this.procesitoGUI.jbt_iniciar) {
+        this.procesitoModelo = new Proceso();
+        this.procesitoGestion = new Gestion();
+        this.procesitoGUI = new ProcesarGUI();
+        this.procesitoGUI.setVisible(true);
+        this.procesitoGUI.jbt_agregar.addActionListener(this);
         this.procesitoGUI.jbt_iniciar.addActionListener(this);
-        System.out.println("Ejecutando...");
-        
-        // Obtener el valor del quantum desde la interfaz de usuario o de donde sea necesario
-        //cuando hagamos el este de que el usuario dije el quamtum pa todo el proceso asi se puede cambiar
-        //pero se debe hacer el getText
-        int quantum = procesitoModelo.getQuantum();
-        // Iniciar la ejecución con el quantum adecuado
-        this.procesitoGestion.iniciarEjecucion(quantum);
-        System.out.println("Ejecución iniciada");
+        this.modeloTabla = (DefaultTableModel) this.procesitoGUI.jtb_lisProcesos.getModel();
     }
-}
 
+    // Eventos de los botones 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == this.procesitoGUI.jbt_agregar) {
+            Proceso nuevoProceso = new Proceso();
+            nuevoProceso.setNombreProceso(this.procesitoGUI.jtf_nombreP.getText());
+            this.procesitoGestion.agregarProceso(nuevoProceso);
+            javax.swing.JOptionPane.showMessageDialog(this.procesitoGUI, "Proceso agregado con éxito: " + nuevoProceso.getNombreProceso());
+            this.procesitoGUI.jtf_nombreP.setText("");
+            this.procesitoGestion.agregarProcesoATabla(modeloTabla, nuevoProceso);
+
+        }
+        
+        if (e.getSource() == this.procesitoGUI.jbt_iniciar) {
+            System.out.println("Ejecutando...");
+            int quantum = this.procesitoModelo.getQuantum();
+            this.procesitoGestion.iniciarEjecucion(quantum);
+            System.out.println("Ejecución iniciada");
+            // Actualizar la tabla después de iniciar la ejecución
+            actualizarTabla();
+        }
+    }
+
+    // Método para actualizar la tabla después de iniciar la ejecución
+    public void actualizarTabla() {
+        modeloTabla.setRowCount(0); // Limpiar la tabla
+        for (Proceso proceso : procesitoGestion.getListaProcesos()) {
+            procesitoGestion.agregarProcesoATabla(modeloTabla, proceso);
+        }
+    }
 
 //gets y sets 
     public ProcesarGUI getProcesitoGUI() {
--- src/main/java/modelo/Gestion.java
+++ src/main/java/modelo/Gestion.java
@@ -9,6 +9,7 @@ import vista.ProcesarGUI;
 public class Gestion {
 
     private List<Proceso> listaProcesos;
+    private DefaultTableModel modeloTabla;
 
     //Constructor
     public Gestion() {
@@ -21,51 +22,64 @@ public class Gestion {
     }
 
     //Esté método posiblemente vaya en el controlador
-public void iniciarEjecucion(int quantum) {
-    int tiempoActual = 0;
+    public void iniciarEjecucion(int quantum) {
+        int tiempoActual = 0;
 
-    while (hayProcesosPendientes()) {
-        for (Proceso proceso : listaProcesos) {
-            if (proceso.getEstado().equals("Listo")) {
-                // Ejecutar una parte de la ráfaga según el quantum
-                int tiempoEjecutado = Math.min(quantum, proceso.getResiduoRafaga());
-                proceso.setResiduoRafaga(proceso.getResiduoRafaga() - tiempoEjecutado);
+        while (hayProcesosPendientes()) {
+            for (Proceso proceso : listaProcesos) {
+                if (proceso.getEstado().equals("Listo")) {
+                    // Ejecutar una parte de la ráfaga según el quantum
+                    int tiempoEjecutado = Math.min(quantum, proceso.getResiduoRafaga());
+                    proceso.setResiduoRafaga(proceso.getResiduoRafaga() - tiempoEjecutado);
 
-                // Actualizar tiempo de ejecución total del proceso
-                proceso.setTiempoejecucion(proceso.getTiempoejecucion() + tiempoEjecutado);
+                    actualizarResiduoRafagaEnTabla(proceso, tiempoEjecutado);
 
-                // Si el proceso ha terminado su ráfaga
-                if (proceso.getResiduoRafaga() == 0) {
-                    proceso.setEstado("Terminado");
-                    proceso.setTiempoTerminacion(tiempoActual + tiempoEjecutado);
-                } else {
-                    proceso.setEstado("Listo");
-                }
+                    // Actualizar tiempo de ejecución total del proceso
+                    proceso.setTiempoejecucion(proceso.getTiempoejecucion() + tiempoEjecutado);
 
-                // Actualizar tiempo de espera de otros procesos
-                for (Proceso otroProceso : listaProcesos) {
-                    if (otroProceso != proceso && otroProceso.getEstado().equals("Listo")) {
-                        otroProceso.setEstado("En espera");
-                        otroProceso.setTiempoEspera(otroProceso.getTiempoEspera() + tiempoEjecutado);
+                    // Si el proceso ha terminado su ráfaga
+                    if (proceso.getResiduoRafaga() == 0) {
+                        proceso.setEstado("Terminado");
+                        proceso.setTiempoTerminacion(tiempoActual + tiempoEjecutado);
+                    } else {
+                        proceso.setEstado("Listo");
                     }
-                }
 
-                // Actualizar tiempo actual
-                tiempoActual += tiempoEjecutado;
+                    // Actualizar tiempo de espera de otros procesos
+                    for (Proceso otroProceso : listaProcesos) {
+                        if (otroProceso != proceso && otroProceso.getEstado().equals("Listo")) {
+                            otroProceso.setEstado("En espera");
+                            otroProceso.setTiempoEspera(otroProceso.getTiempoEspera() + tiempoEjecutado);
+                        }
+                    }
+
+                    // Actualizar tiempo actual
+                    tiempoActual += tiempoEjecutado;
+                }
             }
         }
     }
-}
-        
-public boolean hayProcesosPendientes() {
-    for (Proceso proceso : listaProcesos) {
-        if (proceso.getEstado().equals("Listo") || proceso.getEstado().equals("En espera")) {
-            return true;
+
+    public void actualizarResiduoRafagaEnTabla(Proceso proceso, int tiempoEjecutado) {
+        for (int i = 0; i < modeloTabla.getRowCount(); i++) {
+            if (modeloTabla.getValueAt(i, 0).equals(proceso.getNumeroProceso())) {
+                int residuoActual = (int) modeloTabla.getValueAt(i, 4); // Obtener el residuo actual
+                int nuevoResiduo = residuoActual - tiempoEjecutado; // Calcular el nuevo residuo
+                modeloTabla.setValueAt(nuevoResiduo, i, 4); // Actualizar el valor en la tabla
+                modeloTabla.fireTableDataChanged(); // Notificar cambio en los datos
+                break;
+            }
         }
     }
-    return false;
-}
 
+    public boolean hayProcesosPendientes() {
+        for (Proceso proceso : listaProcesos) {
+            if (proceso.getEstado().equals("Listo") || proceso.getEstado().equals("En espera")) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     public void agregarProcesoATabla(DefaultTableModel modeloTabla, Proceso proceso) {
         Object[] fila = new Object[6];
@@ -118,6 +132,14 @@ public boolean hayProcesosPendientes() {
 
     public void setListaProcesos(List<Proceso> listaProcesos) {
         this.listaProcesos = listaProcesos;
+    }
+
+    public DefaultTableModel getModeloTabla() {
+        return modeloTabla;
+    }
+
+    public void setModeloTabla(DefaultTableModel modeloTabla) {
+        this.modeloTabla = modeloTabla;
     }
 
 }
